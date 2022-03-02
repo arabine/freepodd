bd85a925f6ccf482b7fb6c3e80f78dc04f0764df
Anthony Rabine <anthony@rabine.fr>
2022-03-02 13:44:47 +0100

    fix: manifest, new icons

---
+++ assets/export_icons.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+ 
+inkscape --export-type="png" icon.svg --export-filename=../public/logo.png --export-dpi=300
+inkscape --export-type="png" icon.svg --export-filename=../public/logo48x48.png --export-width=48
+inkscape --export-type="png" icon.svg --export-filename=../public/logo72x72.png --export-width=72
+inkscape --export-type="png" icon.svg --export-filename=../public/logo96x96.png --export-width=96
+inkscape --export-type="png" icon.svg --export-filename=../public/logo144x144.png --export-width=144
+inkscape --export-type="png" icon.svg --export-filename=../public/logo168x168.png --export-width=168
+inkscape --export-type="png" icon.svg --export-filename=../public/logo192x192.png --export-width=192
